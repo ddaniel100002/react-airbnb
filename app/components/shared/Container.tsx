734bf732db5aac19904066dd8fc59e9f0b844052
ddaniel100002 <drc853083@gmail.com>
2023-04-25 15:14:25 +0300

    Navbar from start-to-finish

---
+++ app/components/shared/Container.tsx
@@ -0,0 +1,17 @@
+//we are using this so that we can intergrate this safely with other client components in our app.
+'use client';
+
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({
+    children
+}) => {
+    return (
+        <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
+            {children}
+        </div>
+    )
+}
+export default Container;
